87f6c53c842d110489a91325bdfb3840288fc218
Riley Hall <riley.hall.va@gmail.com>
2022-02-25 15:08:40 -0500

    Add visual buffer to clipping box widget

--- src/zoo/ClippingBox.py
+++ src/zoo/ClippingBox.py
@@ -1,12 +1,14 @@
+import numpy as np
 from vtkmodules.vtkInteractionWidgets import vtkBoxWidget2
 
 
 class ClippingBox(vtkBoxWidget2):
-    def __init__(self, model, interactor) -> None:
+    def __init__(self, model, interactor, buffer_factor: float = 0.01) -> None:
         super().__init__()
+        self.buffer_factor = buffer_factor
         self.model = model
         self.SetInteractor(interactor)
-        self.GetRepresentation().SetPlaceFactor(1)
+        self.GetRepresentation().SetPlaceFactor(1.0)
         self.GetRepresentation().SetOutlineCursorWires(False)
         self.SetRotationEnabled(False)
         self.SetTranslationEnabled(False)
@@ -16,7 +18,11 @@ class ClippingBox(vtkBoxWidget2):
         self.AddObserver("InteractionEvent", self.callback)
 
     def update(self, bounds):
-        self.GetRepresentation().PlaceWidget(bounds)
+        self.GetRepresentation().PlaceWidget(
+            np.array(bounds) * (1.0 + self.buffer_factor)
+        )
 
     def callback(self, obj, event):
-        self.model._set_clipping_extents(self.GetRepresentation().GetBounds())
+        self.model._set_clipping_extents(
+            np.array(self.GetRepresentation().GetBounds()) / (1.0 + self.buffer_factor)
+        )
