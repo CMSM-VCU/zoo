e28cdf45c9cf8137326433c2d9cf8bb291342765
Riley Hall <riley.hall.va@gmail.com>
2022-03-10 18:03:15 -0500

    Add matplotlib package to requirements

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setuptools.setup(
         "scipy",
         "pyperclip",
         "pywin32",
-        "colorcet",
+        "matplotlib" "colorcet",
     ],
     entry_points={"console_scripts": ["zoo = zoo:run"]},
 )
