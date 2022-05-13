4a710bff75319727afc26d12d229e8bc83a2e78d
Riley Hall <riley.hall.va@gmail.com>
2022-05-13 12:42:35 -0400

    Bump version number to 0.8.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zoo",
-    version="0.8.0",
+    version="0.8.1",
     author="Riley Hall",
     author_email="riley.hall.va@gmail.com",
     description="A tool for visualizing Peridynamic simulation results",
