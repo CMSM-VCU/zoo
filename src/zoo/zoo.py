a1df8cbca415cd8325c8e0334e057c1ec599294f
Riley Hall <riley.hall.va@gmail.com>
2022-07-26 10:35:19 -0400

    Convert to using `argparse`

--- src/zoo/zoo.py
+++ src/zoo/zoo.py
@@ -1,4 +1,5 @@
 import sys
+from argparse import ArgumentParser
 from pathlib import Path
 
 from qtpy import QtCore, QtWidgets
@@ -6,19 +7,27 @@ from qtpy import QtCore, QtWidgets
 from .MainWindow import MainWindow
 
 
+def path_is_file(path: str) -> Path | None:
+    """Convert the string to a path only if it is a path to a file. Not using
+    `argparse.FileType` because we don't want to actually use the file yet.
+
+    Args:
+        path (str): Path string to be checked
+
+    Returns:
+        Path | None: Path to file, if it exists
+    """
+    return Path(path) if Path(path).is_file() else None
+
+
 def run():
+    parser = ArgumentParser()
+    parser.add_argument("file", nargs="?", help="The file to open", type=path_is_file)
+    args = parser.parse_args()
+
     QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, on=True)
     app = QtWidgets.QApplication(sys.argv)
-    try:
-        file_to_load = Path(sys.argv[1])
-        assert file_to_load.is_file()
-    except IndexError:
-        file_to_load = None
-    except:
-        print(f"Could not find a file at {sys.argv[1]}")
-        file_to_load = None
-    finally:
-        window = MainWindow(file_to_load=file_to_load)
+    window = MainWindow(file_to_load=args.file)
     sys.exit(app.exec_())
 
 
